741a9f98eccef9bbc681efdd6f9bd1ca2dc04109
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-07-01 08:57:33 -0400

    updated js

--- SteamSummerSale/script/index.js
+++ SteamSummerSale/script/index.js
@@ -1,31 +1,24 @@
 let sunrise = 8;
 let sunset = 20;
 let oldDay, oldDate, oldTime;
-let date = new Date();
-const hour = date.getHours();
-let backgroundImage = "";
-if (hour >= sunrise && hour < sunset) {
-  document.body.setAttribute('class', 'day');
-  backgroundImage = "./assets/home_header_bg_day_japanese.gif";
-} else {
-  document.body.setAttribute('class', 'night');
-  backgroundImage = "./assets/home_header_bg_night_japanese.gif"
-}
-
-const image = new Image();
-image.addEventListener('load', function () {
-  document.querySelector('.hero').style.backgroundImage = 'url(' + backgroundImage + ')';
-  document.querySelector('.scan').classList.add("animate");
-});
-image.src = backgroundImage;
 
 
-const timeOptions = { hour: '2-digit', minute: '2-digit', hour12:true };
+const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: true };
 const dateOptions = { month: 'short', year: 'numeric', day: '2-digit' };
 const dayOptions = { weekday: 'long' };
 
 function setDate() {
-  date = new Date();
+  const date = new Date();
+  const hour = date.getHours();
+
+  if (hour >= sunrise && hour < sunset && !document.body.className.includes('day')) {
+    document.body.className = "day"
+    setBackground("./assets/home_header_bg_day_japanese.gif")
+  } else if(!(hour >= sunrise && hour < sunset) && !document.body.className.includes('night')) {
+    document.body.className = "night"
+    setBackground("./assets/home_header_bg_night_japanese.gif")
+  }
+
   const newTime = date.toLocaleDateString(undefined, timeOptions).split(',')[1];
   const newDate = date.toLocaleDateString(undefined, dateOptions);
   const newDay = date.toLocaleDateString(undefined, dayOptions);
@@ -42,6 +35,16 @@ function setDate() {
     document.querySelector('.weekDay').innerText = newDay;
     oldDay = newDay
   }
+}
+
+function setBackground(src){
+  document.querySelector('.scan').classList.remove("animate")
+  const image = new Image();
+  image.addEventListener('load', function () {
+    document.querySelector('.hero').style.backgroundImage = 'url(' + src + ')';
+    document.querySelector('.scan').classList.add("animate");
+  });
+  image.src = src;
 }
 
 setInterval(() => setDate(), 30 * 1000);
